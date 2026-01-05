87f05838fdd92199092b4c6a416aa818eb251776
whua898 <whua898@gmail.com>
2026-01-05 15:27:17 +0800

    wh

--- src/generator/config/subexport.h
+++ src/generator/config/subexport.h
@@ -74,5 +74,6 @@ std::string proxyToQuan(std::vector<Proxy> &nodes, const std::string &base_conf,
 void proxyToQuan(std::vector<Proxy> &nodes, INIReader &ini, std::vector<RulesetContent> &ruleset_content_array, const ProxyGroupConfigs &extra_proxy_group, extra_settings &ext);
 std::string proxyToSSD(std::vector<Proxy> &nodes, std::string &group, std::string &userinfo, extra_settings &ext);
 std::string proxyToSingBox(std::vector<Proxy> &nodes, const std::string &base_conf, std::vector<RulesetContent> &ruleset_content_array, const ProxyGroupConfigs &extra_proxy_group, extra_settings &ext);
+void proxyToSingBox(std::vector<Proxy> &nodes, rapidjson::Document &json, std::vector<RulesetContent> &ruleset_content_array, const ProxyGroupConfigs &extra_proxy_group, extra_settings &ext);
 void replaceAll(std::string& input, const std::string& search, const std::string& replace);
 #endif // SUBEXPORT_H_INCLUDED
